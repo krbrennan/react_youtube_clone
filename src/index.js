d8e8724a302e9b808ab6bf11d98fb8d4ac6b98f3
krbrennan <krb.brennan@gmail.com>
2018-06-07 17:43:39 -0400

    idk what im doing here

---
+++ src/index.js
@@ -0,0 +1,52 @@
+// Create component and have it produce some HTML
+
+// Take this component's generated HTML and put it in the DOM
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
+import VideoListItem from './components/video_list_item';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyCf3JVq5QRqy4AiKvOwAgRvmVmXC4WSU5E';
+
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('vsauce');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term }, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+     });
+  });
+
+}
+
+  render () {
+    return (
+      <div>
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
